096f3d387fc02a767bd72a1bccf829665d6a1418
AdibLeon <162968531+AdibLeon@users.noreply.github.com>
2024-03-12 16:35:44 +0700

    Add files via upload

--- Tugas Salary Adib.py
+++ Tugas Salary Adib.py
@@ -6,10 +6,12 @@ def main():
     nama_karyawan = input("Nama karyawan: ")
     gaji_pokok = float(input("Masukkan gaji pokok: "))
     jam_kerja = int(input("Masukkan Jam Kerja Harian: "))
-    
+    jumlah_hari_kerja_per_minggu = int(input("Masukkan Jam Kerja Mingguan: "))
+
     print("\nNama karyawan:", nama_karyawan)
     print("Gaji pokok:", "Rp.",gaji_pokok)
     print("Jumlah jam kerja per hari:", jam_kerja,"Jam")
+    print("Jumlah hari kerja per minggu:", jumlah_hari_kerja_per_minggu,"hari")
     print("\nPilih Rincian Gaji Mingguan/Harian:")
     print("1. Gaji Harian")
     print("2. Gaji Mingguan")
@@ -19,8 +21,6 @@ def main():
         gaji_harian = gaji_pokok / jam_kerja
         print("Gaji harian:", "Rp.",gaji_harian)
     elif pilihan == 2:
-        jumlah_hari_kerja_per_minggu = int(input("Masukkan Jam Kerja Mingguan: "))
-        print("Jumlah hari kerja per minggu:", jumlah_hari_kerja_per_minggu,"hari")
         gaji_mingguan = gaji_pokok / (jam_kerja * jumlah_hari_kerja_per_minggu)
         print("Gaji mingguan:", "Rp.",gaji_mingguan)
     else:
