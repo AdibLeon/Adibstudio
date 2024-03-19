14ee68ae63a566d452277da129d4c18fe5df4e6d
M.Adib Aliffathan <162968531+AdibLeon@users.noreply.github.com>
2024-03-19 17:07:15 +0900

    Add files via upload

---
+++ Tugas konversi .py
@@ -0,0 +1,10 @@
+dec = int(input("Masukkan bilangan desimal: "))
+
+biner = bin(dec)
+print("Hasil konversi ke biner:", biner)
+
+oktal = oct(dec)
+print("Hasil konversi ke oktal:", oktal)
+
+heksa = hex(dec)
+print("Hasil konversi ke heksadesimal:", heksa)
